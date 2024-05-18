f90b31fa6fa48cedac5064ddf41d65d95cac5e4f
Pluhec <jakplu@seznam.cz>
2024-05-18 15:19:55 +0200

    Jazyk hotov, neresponzivni contact. bla bla

---
+++ clock.js
@@ -0,0 +1,7 @@
+let hrs = document.getElementById("hrs");
+let min = document.getElementById("min");
+let sec = document.getElementById("sec");
+
+// hrs.innerHTML = currentTime.getHours()
+
+console.log = ('argentina', '-3')
