9bc7309058034cfe8002b600334623b3951a9ca2
Pluhec <jakplu@seznam.cz>
2024-05-18 15:19:57 +0200

    asfda

--- darkmode.js
+++ darkmode.js
@@ -1,35 +1,40 @@
 let darkMode = localStorage.getItem('darkMode'); // ulozeni do uzivatelova prohlizece, aby kdyz znovu nacte web tak aby se mu zobralil mode ktery si predtim nastavil
 const darkModeToggle = document.querySelector('#dark-mode-toggle'); // vythanuti z tagu z HTML
-const darkImg = document.getElementsByClassName('link-icon') // vythanuti z tagu z HTML
+const modeIcon = document.getElementById('dark-mode-toggle');
+const jpLogo = document.getElementById('jp-logo');
+const darkImg = document.getElementsByClassName('link-icon');
 
 const enableDarkMode = () => { 
-  document.body.classList.add('darkmode'); // pridavani classy
+  document.body.classList.add('darkmode'); // pridavani classy do body
   localStorage.setItem('darkMode', 'enabled'); // ukladani promene do local storage
-  darkModeToggle.innerHTML = "Dark mode"; // zmena napisu pri zmeneni barvy
+  modeIcon.src = "Icons/night-mode.png";
+  jpLogo.src = "Icons/jp-logo-black.png";
+
   for (let i = 0; i< darkImg.length; i++) { // menim ikonu tim ze projedu to pole a zmenim kazdy img v poli
-    darkImg[i].src = ('Icons/link-black.png')
+    darkImg[i].src = ('Icons/link-black.png');
   }
-  for (let i = 0; i< darkImg.length; i++) { // menim ikonu tim ze projedu to pole a zmenim kazdy img v poli
-    darkImg[i].src = ('Icons/link-black.png')
-  }
-}
+};
 
 const disableDarkMode = () => {
   document.body.classList.remove('darkmode');
   localStorage.setItem('darkMode', null);
-  darkModeToggle.innerHTML = "Light mode"; 
+  modeIcon.src = "Icons/sun.png";
+  jpLogo.src = "Icons/jp-logo-white.png";
 
   for (let i = 0; i< darkImg.length; i++) { 
-    darkImg[i].src = ('Icons/link-white.png')
+    darkImg[i].src = ('Icons/link-white.png');
   }
-}
+};
 
-if (darkMode === 'enabled') {
+if (localStorage.getItem('darkMode') === 'enabled') {
   enableDarkMode();
+}else {
+  disableDarkMode();
+
 }
 
 darkModeToggle.addEventListener('click', () => {
-  darkMode = localStorage.getItem('darkMode');
+  const darkMode = localStorage.getItem('darkMode');
 
   if (darkMode !== 'enabled') {
     enableDarkMode();
