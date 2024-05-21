4e375a7128f51e9618e24983e3ab7bd93a9f736b
Pluhec <jakplu@seznam.cz>
2024-05-21 22:48:45 +0200

    idk

--- clock.js
+++
@@ -1,7 +0,0 @@
-let hrs = document.getElementById("hrs");
-let min = document.getElementById("min");
-let sec = document.getElementById("sec");
-
-// hrs.innerHTML = currentTime.getHours()
-
-console.log = ('argentina', '-3')
